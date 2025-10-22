b05120ea45a88b8bc296c2b6934bcf3860ce6027
rairei <raireipq@gmail.com>
2025-10-22 20:47:29 +0700

    some updates to wt profiles

--- pwsh/profile.ps1
+++ pwsh/profile.ps1
@@ -1,14 +1,14 @@
-clear-host
+Clear-Host
 
-oh-my-posh init pwsh --eval --config ~/.config/ohmyposh/ohmyposh.omp.json | Invoke-Expression
+oh-my-posh init pwsh --config ~/.config/ohmyposh/ohmyposh.omp.json | Invoke-Expression
 
-fastfetch --logo windows --logo-padding-top 1
+# fastfetch --logo windows --logo-padding-top 1
 
 # Set the console title to the currently running command
 Set-PSReadLineKeyHandler -Key Enter `
-                         -BriefDescription RunWithTitle `
-                         -LongDescription "Set the console title to the command, then run the command" `
-                         -ScriptBlock {
+    -BriefDescription RunWithTitle `
+    -LongDescription "Set the console title to the command, then run the command" `
+    -ScriptBlock {
     param($key, $arg)
 
     $line = $null
@@ -21,37 +21,53 @@ Set-PSReadLineKeyHandler -Key Enter `
 function conit {
     # Conda initialization
     If (Test-Path "C:\Users\rairei\miniconda3\Scripts\conda.exe") {
-        (& "C:\Users\rairei\miniconda3\Scripts\conda.exe" "shell.powershell" "hook") | Out-String | ?{$_} | Invoke-Expression
+        (& "C:\Users\rairei\miniconda3\Scripts\conda.exe" "shell.powershell" "hook") | Out-String | ? { $_ } | Invoke-Expression
         conda deactivate
-  	oh-my-posh init pwsh --config ~/.config/ohmyposh/ohmyposh.omp.json | Invoke-Expression
+
+        oh-my-posh init pwsh --config ~/.config/ohmyposh/ohmyposh.omp.json | Invoke-Expression
     }
 }
 
-function cdnvim {
-	set-location $env:LOCALAPPDATA\nvim
-  }
+function touch {
+    param(
+        [Parameter(Mandatory = $true)]
+        [string]$Path
+    )
 
-function cdome {
-	set-location ~
-  }
+    if (-not (Test-Path $Path)) {
+        New-Item -Path $Path -ItemType File | Out-Null
+    }
 
-function cdonfig {
-	set-location ~\.config
+    Set-ItemProperty -Path $Path -Name LastAccessTime -Value (Get-Date)
 }
 
-function cdappdata {
-	set-location $env:LOCALAPPDATA
+function codef {
+    $selected = fd -tf -u . $resolvedPath | fzf
+    if ($selected) {
+        code $selected
+    }
 }
 
-function cdprogramfiles {
-	set-location $env:PROGRAMFILES
+function codec {
+    $selected = fd -td -u . $resolvedPath | fzf
+    if ($selected) {
+        code $selected
+    }
 }
 
-function cdprj {
-	set-location ~\projects
+function cdc {
+    $selected = fd -td -u | fzf
+    if ($selected) {
+        Set-Location $selected
+    }
 }
 
-function cdkm {
-	set-location $env:LOCALAPPDATA\keymapper
-}
+function cdnvim { Set-Location $env:LOCALAPPDATA\nvim }
 
+function cdappdata { Set-Location $env:LOCALAPPDATA }
+
+function cdprogramfiles { Set-Location $env:PROGRAMFILES }
+
+function cdkm { Set-Location $env:LOCALAPPDATA\keymapper }
+
+function ffetch { fastfetch --logo windows --logo-padding-top 1 }
